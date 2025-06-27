6ef83ac42cefc6e3435e5edc5c391639e2448dd9
anudnya-tado <anushtybhilare@gmail.com>
2025-06-27 16:38:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
+# MongoDB connection
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://Anudnya:anudnya1@anns.cvxkuxx.mongodb.net/loginDB?retryWrites=true&w=majority")
+client = MongoClient(MONGO_URI)
+db = client['loginDB']
+users = db['users']  # Make sure your collection is named 'users'
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index1.html')  # Make sure this file is in the templates folder
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    user = users.find_one({'username': username, 'password': password})
+    if user:
+        return f"✅ Welcome, {username}!"
+    else:
+        return "❌ Invalid username or password"
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(debug=True)
